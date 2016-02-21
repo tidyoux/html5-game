68fa976bdd6abb9187343644f5ebb9c29b6145ed
tidyoux <313247557@qq.com>
2016-02-21 09:09:00 +0800

    初始提交。

---
+++ src/base.js
@@ -0,0 +1,392 @@
+﻿
+function Point(x, y) {
+	this.x = x;
+	this.y = y;
+}
+
+function Size(width, height) {
+	this.width = width;
+	this.height = height;
+}
+
+function Rect(position, size) {
+	this.position = position;
+	this.size = size;
+
+	this.containPoint = function(point) {
+		var pt = this.position || new Point(0, 0);
+		var sz = this.size || new Size(0, 0);
+		if ((pt.x < point.x) && (point.x < pt.x + size.width) &&
+				(pt.y < point.y) && (point.y < pt.y + size.height))
+		{
+			return true;
+		}
+		return false;
+	}
+}
+
+function ViewContainer(render) {
+	this.name = "";
+	this.render = render;
+	this.size = new Size(0, 0);
+	this.position = new Point(0, 0);
+	this.scale = new Point(1, 1);
+	this.isClippingEnabled = false;
+	this.clippingRect = new Rect(new Point(0, 0), new Size(0, 0));
+	this.children = new Array();
+	this.parent = null;
+	this.isTouchEnabled = false;
+	this.isTouched = false;
+
+	this.setName = function(name) {
+		this.name = name;
+	}
+	this.getName = function() {
+		return this.name;
+	}
+	this.setWidth = function(width) {
+		this.size.width = width;
+	}
+	this.getWidth = function() {
+		return this.size.width;
+	}
+	this.setHeight = function(height) {
+		this.size.height = height;
+	}
+	this.getHeight = function() {
+		return this.size.height;
+	}
+	this.setPositionX = function(x) {
+		this.position.x = x;
+	}
+	this.getPositionX = function() {
+		return this.position.x;
+	}
+	this.setPositionY = function(y) {
+		this.position.y = y;
+	}
+	this.getPositionY = function() {
+		return this.position.y;
+	}
+	this.setScaleX = function(sx) {
+		this.scale.x = sx;
+	}
+	this.getScaleX = function() {
+		return this.scale.x;
+	}
+	this.setScaleY = function(sy) {
+		this.scale.y = sy;
+	}
+	this.getScaleY = function() {
+		return this.scale.y;
+	}
+	this.setClippingEnabled = function(isEnabled) {
+		this.isClippingEnabled = isEnabled;
+	}
+	this.getClippingEnabled = function() {
+		return this.isClippingEnabled;
+	}
+	this.setClippingRect = function(rect) {
+		this.clippingRect = rect;
+	}
+	this.getClippingRect = function() {
+		return this.clippingRect;
+	}
+	this.addChild = function(child) {
+		this.children.push(child);
+		child.setParent(this);
+	}
+	this.getChild = function(name) {
+		if (name == "") {
+			return null;
+		}
+
+		for (var i = 0; i < this.children.length; i++) {
+			var child = this.children[i];
+			if (child.getName() == name) {
+				return child;
+			}
+		}
+		return null;
+	}
+	this.removeChild = function(child) {
+		if (child == null) {
+			return;
+		}
+		
+		for (var i = 0; i < this.children.length; i++) {
+			if (this.children[i] == child) {
+				child.setParent(null);
+				this.children.splice(i, 1);
+				break;
+			}
+		}
+	}
+	this.removeChildByName = function(name) {
+		if (name == "") {
+			return;
+		}
+
+		for (var i = 0; i < this.children.length; i++) {
+			var child = this.children[i];
+			if (child.getName() == name) {
+				child.setParent(null);
+				this.children.splice(i, 1);
+				break;
+			}
+		}
+	}
+	this.removeFromParent = function() {	
+		if (this.parent != null) {
+			this.parent.removeChild(this);
+		}
+	}
+	this.setParent = function(parent) {
+		this.parent = parent;
+	}
+	this.getParent = function() {
+		return this.parent;
+	}
+	this.setTouchEnabled = function(isEnabled) {
+		this.isTouchEnabled = isEnabled;
+	}
+	this.getTouchEnabled = function() {
+		return this.isTouchEnabled;
+	}
+	this.update = function(dt) {
+		this.doUpdate(dt);
+		for (var i = 0; i < this.children.length; i++) {
+			this.children[i].update(dt);
+		}
+	}
+	this.doUpdate = function(dt) {
+		//
+	}
+	this.draw = function() {
+		var sx = this.getScaleX();
+		var sy = this.getScaleY();
+		this.render.scale(sx, sy);
+		if (this.isClippingEnabled) {
+			this.render.save();
+
+			var r = this.clippingRect;
+			this.render.rect(r.position.x, r.position.y, r.size.width, r.size.height);
+			this.render.clip();
+		}
+
+		//
+		this.doDraw();
+		for (var i = 0; i < this.children.length; i++) {
+			this.children[i].draw();
+		}
+
+		//
+		if (this.isClippingEnabled) {
+			this.render.restore();
+		}
+		this.render.scale(1 / sx, 1 / sy);
+	}
+	this.doDraw = function() {
+		//
+	}
+	this.onTouchBegan = function(event) {
+		this.isTouched = false;
+		for (var i = (this.children.length - 1); i >= 0; i--) {
+			var child = this.children[i];
+			if (child.onTouchBegan(event)) {
+				return true;
+			}
+		}
+
+		if (this.isTouchEnabled && this.containTouch(event)) {
+			this.doTouchBegan(event);
+			this.isTouched = true;
+		}
+		return this.isTouched;
+	}
+	this.doTouchBegan = function(event) {
+		//
+	}
+	this.onTouchMoved = function(event) {
+		if (this.isTouched) {
+			this.doTouchMoved(event);
+		} else {
+			for (var i = (this.children.length - 1); i >= 0; i--) {
+				this.children[i].onTouchMoved(event);
+			}
+		}
+	}
+	this.doTouchMoved = function(event) {
+		//
+	}
+	this.onTouchEnded = function(event) {
+		if (this.isTouched) {
+			this.isTouched = false;
+			this.doTouchEnded(event);
+		} else {
+			for (var i = (this.children.length - 1); i >= 0; i--) {
+				this.children[i].onTouchEnded(event);
+			}
+		}
+	}
+	this.doTouchEnded = function(event) {
+		//
+	}
+	this.containTouch = function(event) {
+		var rect = new Rect(this.position, this.size);
+		return rect.containPoint(new Point(event.layerX, event.layerY));
+	}
+}
+
+function ImageView(render, imageName) {
+	ViewContainer.call(this, render);
+	this.image = "img/" + imageName;
+
+	this.setImage = function(imageName) {
+		this.image = "img/" + imageName;
+	}
+	this.doDraw = function() {
+		var img = new Image();
+		img.src = this.image;
+
+		var x = this.getPositionX();
+		var y = this.getPositionY();
+		var w = this.getWidth();
+		var h = this.getHeight();
+		if (w > 0 && h > 0) {
+			this.render.drawImage(img, x, y, w, h);
+		} else {
+			this.render.drawImage(img, x, y);
+		}
+	}
+}
+
+function TextView(render, txt) {
+	ViewContainer.call(this, render);
+	this.txt = txt;
+	this.font = render.font;
+	this.fillStyle = render.fillStyle;
+
+	this.setText = function(txt) {
+		this.txt = txt;
+	}
+	this.setFont = function(font) {
+		this.font = font;
+	}
+	this.setStyle = function(style) {
+		this.fillStyle = style;
+	}
+	this.doDraw = function() {
+		var defaultFont = this.render.font;
+		var defaultStyle = this.render.fillStyle;
+
+		var x = this.getPositionX();
+		var y = this.getPositionY();
+		this.render.font = this.font || defaultFont;
+		this.render.fillStyle = this.fillStyle || defaultStyle; 
+		this.render.fillText(this.txt, x, y);
+
+		this.render.font = defaultFont;
+		this.render.fillStyle = defaultStyle;
+	}
+}
+
+function ButtonView(render, normalImage, selectImage, disableImage) {
+	ViewContainer.call(this, render);
+	this.normalImage = "img/" + normalImage;
+	if (selectImage != null) {
+		this.selectImage = "img/" + selectImage;
+		if (disableImage != null) {
+			this.disableImage = "img/" + disableImage;
+		} else {
+			this.disableImage = this.normalImage;
+		}
+	} else {
+		this.selectImage = this.normalImage;
+		this.disableImage = this.normalImage;
+	}
+	this.currentImage = this.normalImage;
+	this.touchBeganHandler = null;
+	this.touchEndedHandler = null;
+
+	this.setTouchEnabled = function(isEnabled) {
+		this.isTouchEnabled = isEnabled;
+		if (isEnabled) {
+			this.currentImage = this.normalImage;
+		} else {
+			this.currentImage = this.disableImage;
+		}
+	}
+	this.setTouchBeganHandler = function(handler) {
+		this.touchBeganHandler = handler;
+	}
+	this.setTouchEndedHandler = function(handler) {
+		this.touchEndedHandler = handler;
+	}
+	this.doDraw = function() {
+		var img = new Image();
+		img.src = this.currentImage;
+
+		var x = this.getPositionX();
+		var y = this.getPositionY();
+		var w = this.getWidth();
+		var h = this.getHeight();
+		if (w > 0 && h > 0) {
+			this.render.drawImage(img, x, y, w, h);
+		} else {
+			this.render.drawImage(img, x, y);
+		}
+	}
+	this.doTouchBegan = function(event) {
+		this.currentImage = this.selectImage;
+		if (this.touchBeganHandler != null) {
+			this.touchBeganHandler();
+		}
+	}
+	this.doTouchEnded = function(event) {
+		this.currentImage = this.normalImage;
+		if (this.containTouch(event)) {
+			if (this.touchEndedHandler != null) {
+				this.touchEndedHandler();
+			}
+		}
+	}
+}
+
+function Stage(render) {
+	ViewContainer.call(this, render);
+}
+
+function World(width, height, interval) {
+	interval = interval || 0.05;
+	this.timer = null;
+	this.size = new Size(width, height);
+	this.render = document.getElementById('myCanvas').getContext('2d'); 
+	this.stage = new Stage(this.render);
+
+	var self = this;
+	document.onmousedown = function(event) {
+		self.stage.onTouchBegan(event);
+	}
+	document.onmousemove = function(event) {
+		self.stage.onTouchMoved(event);
+	}
+	document.onmouseup = function(event) {
+		self.stage.onTouchEnded(event);
+	}
+
+	this.update = function(dt) {
+		this.stage.update(dt);
+
+		this.render.clearRect(0, 0, this.size.width, this.size.height);
+		this.stage.draw();
+	}
+	this.run = function() {
+		this.timer = setInterval(
+				function() {
+					self.update(interval);
+				}, 1000 * interval);
+	}
+	this.run();
+}
+
---
+++ src/game.js
@@ -0,0 +1,198 @@
+﻿
+function GameWorld(width, height) {
+	World.call(this, width, height);
+	var self = this;
+	var render = this.render;
+	var stage = this.stage;
+
+	this.init = function() {
+		this.initBkg();
+		this.initTitle();
+		this.initBody();
+	}
+
+	this.initBkg = function() {
+		var bkg = new ImageView(render, "bkg.jpg");
+		bkg.setWidth(this.size.width);
+		bkg.setHeight(this.size.height);
+		stage.addChild(bkg);
+	}
+
+	this.initTitle = function() {
+		var titleBoard = new ImageView(render, "title.png");
+		titleBoard.setWidth(this.size.width);
+		titleBoard.setHeight(40);
+		stage.addChild(titleBoard);
+
+		var backBtn = new ButtonView(render, "btn_return.png");
+		backBtn.setWidth(20);
+		backBtn.setHeight(20);
+		backBtn.setPositionX(15);
+		backBtn.setPositionY(10);
+		titleBoard.addChild(backBtn);
+
+		var titleLabel = new TextView(render, "开心摇摇乐");
+		titleLabel.setFont("16px 黑体");
+		titleLabel.setPositionX(50);
+		titleLabel.setPositionY(28);
+		titleBoard.addChild(titleLabel);
+
+		var refreshBtn = new ButtonView(render, "btn_refresh.png");
+		refreshBtn.setWidth(20);
+		refreshBtn.setHeight(20);
+		refreshBtn.setPositionX(210);
+		refreshBtn.setPositionY(10);
+		titleBoard.addChild(refreshBtn);
+
+		var shareBtn = new ButtonView(render, "btn_share.png");
+		shareBtn.setWidth(20);
+		shareBtn.setHeight(20);
+		shareBtn.setPositionX(250);
+		shareBtn.setPositionY(10);
+		titleBoard.addChild(shareBtn);
+
+		var moreBtn = new ButtonView(render, "btn_more_op.png");
+		moreBtn.setWidth(6);
+		moreBtn.setHeight(20);
+		moreBtn.setPositionX(290);
+		moreBtn.setPositionY(10);
+		titleBoard.addChild(moreBtn);
+
+		var ruleBoard = new ImageView(render, "rule_board.png");
+		ruleBoard.setWidth(80);
+		ruleBoard.setHeight(20);
+		ruleBoard.setPositionX(this.size.width - 80);
+		ruleBoard.setPositionY(40);
+		titleBoard.addChild(ruleBoard);
+
+		var ruleLabel = new TextView(render, "查看游戏规则");
+		ruleLabel.setFont("12px 黑体");
+		ruleLabel.setStyle("#ffffff");
+		ruleLabel.setPositionX(ruleBoard.getPositionX() + 5);
+		ruleLabel.setPositionY(ruleBoard.getPositionY() + 16);
+		ruleBoard.addChild(ruleLabel);
+	}
+
+	this.initBody = function() {
+		var costLabel = new TextView(render, "10金币可以玩一次");
+		costLabel.setFont("10px 黑体");
+		costLabel.setStyle("#ffff00");
+		costLabel.setPositionX(115);
+		costLabel.setPositionY(142);
+		stage.addChild(costLabel);
+
+		var startPt = new Point(50, 210);
+		var ox = 73;
+		for (var i = 0; i < 3; i++) {
+			var img = new ImageView(render, "num_7.png");
+			img.setWidth(70);
+			img.setHeight(90);
+			img.setPositionX(startPt.x + i * ox);
+			img.setPositionY(startPt.y);
+			stage.addChild(img);
+		}
+
+		var noteLabel = new TextView(render, "点击PLAY，开始摇奖，祝您好运！");
+		noteLabel.setFont("14px 黑体");
+		noteLabel.setStyle("#ffffff");
+		noteLabel.setPositionX(60);
+		noteLabel.setPositionY(355);
+		stage.addChild(noteLabel);
+
+		var playBtn = new ButtonView(render, "btn_play_norm.png", "btn_play_sel.png", "btn_play_dis.png");
+		playBtn.setWidth(150);
+		playBtn.setHeight(60);
+		playBtn.setPositionX(85);
+		playBtn.setPositionY(448);
+		playBtn.setTouchEnabled(true);
+		playBtn.setTouchEndedHandler(
+				function() {
+					self.playBtn.setTouchEnabled(false);
+					self.startRandom();
+				});
+		stage.addChild(playBtn);
+		this.playBtn = playBtn;
+
+		var rewardListLabel = new TextView(render, "获奖！恭喜唔咔咔获奖！恭喜小");
+		rewardListLabel.setFont("16px 黑体");
+		rewardListLabel.setStyle("#ffffff");
+		rewardListLabel.setPositionX(50);
+		rewardListLabel.setPositionY(580);
+		stage.addChild(rewardListLabel);
+	}
+
+	this.startRandom = function() {
+		//
+		if (true) {
+			this.showPopPanel();
+		}
+	}
+
+	this.showPopPanel = function() {
+		var bkg = new ImageView(render, "rule_board.png");
+		bkg.setWidth(this.size.width);
+		bkg.setHeight(this.size.height);
+		bkg.setTouchEnabled(true);
+		stage.addChild(bkg);
+
+		var board = new ImageView(render, "pop_board.png");
+		board.setWidth(280);
+		board.setHeight(350);
+		board.setPositionX(bkg.getWidth() / 2 - board.getWidth() / 2);
+		board.setPositionY(bkg.getHeight() / 2 - board.getHeight() / 2);
+		bkg.addChild(board);
+
+		var closeBtn = new ButtonView(render, "btn_close.png");
+		closeBtn.setWidth(40);
+		closeBtn.setHeight(40);
+		closeBtn.setPositionX(board.getPositionX() + board.getWidth() - closeBtn.getWidth() / 2 - 10);
+		closeBtn.setPositionY(board.getPositionY() - closeBtn.getHeight() / 2 + 12);
+		closeBtn.setTouchEnabled(true);
+		closeBtn.setTouchEndedHandler(
+				function() {
+					bkg.removeFromParent();
+					self.playBtn.setTouchEnabled(true);
+				});
+		board.addChild(closeBtn);
+
+		var noteLabel1 = new TextView(render, "哎呀，差点就中了！");
+		noteLabel1.setFont("24px 黑体");
+		noteLabel1.setPositionX(board.getPositionX() + 40);
+		noteLabel1.setPositionY(board.getPositionY() + 80);
+		board.addChild(noteLabel1);
+
+		var icon = new ImageView(render, "img_2.png");
+		icon.setWidth(120);
+		icon.setHeight(120);
+		icon.setPositionX(board.getPositionX() + board.getWidth() / 2 - icon.getWidth() / 2);
+		icon.setPositionY(board.getPositionY() + board.getHeight() / 2 - icon.getHeight() / 2 - 10);
+		board.addChild(icon);
+
+		var againBtn = new ButtonView(render, "btn_again.png");
+		againBtn.setWidth(150);
+		againBtn.setHeight(50);
+		againBtn.setPositionX(board.getPositionX() + board.getWidth() / 2 - againBtn.getWidth() / 2);
+		againBtn.setPositionY(icon.getPositionY() + 150);
+		againBtn.setTouchEnabled(true);
+		againBtn.setTouchEndedHandler(
+				function() {
+					bkg.removeFromParent();
+					self.playBtn.setTouchEnabled(true);
+				});
+		board.addChild(againBtn);
+
+		var againLabel = new TextView(render, "再来一次");
+		againLabel.setFont("20px 黑体");
+		againLabel.setStyle("#ffffff");
+		againLabel.setPositionX(againBtn.getPositionX() + 35);
+		againLabel.setPositionY(againBtn.getPositionY() + 32);
+		againBtn.addChild(againLabel);
+	}
+}
+
+//
+function start() {
+	var world = new GameWorld(320, 640);
+	world.init();
+}
+start();
